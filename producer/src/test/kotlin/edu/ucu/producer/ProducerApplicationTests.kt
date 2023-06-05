857b6518282cda6117248954199ab094c3be854c
Yaroslav Borodaienko <yaroslaww1@gmail.com>
2023-06-05 22:17:46 +0100

    Producer: Initial commit

---
+++ producer/src/test/kotlin/edu/ucu/producer/ProducerApplicationTests.kt
@@ -0,0 +1,13 @@
+package edu.ucu.producer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProducerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
