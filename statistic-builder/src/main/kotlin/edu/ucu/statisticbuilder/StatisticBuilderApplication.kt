a782615958c31c7282c6ca5038dcbe6884d9920d
Yaroslav Borodaienko <yaroslaww1@gmail.com>
2023-06-09 21:17:30 +0100

    StatisticBuilder: Initial commit

---
+++ statistic-builder/src/main/kotlin/edu/ucu/statisticbuilder/StatisticBuilderApplication.kt
@@ -0,0 +1,11 @@
+package edu.ucu.statisticbuilder
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StatisticBuilderApplication
+
+fun main(args: Array<String>) {
+    runApplication<StatisticBuilderApplication>(*args)
+}
---
+++ statistic-builder/src/test/kotlin/edu/ucu/statisticbuilder/StatisticBuilderApplicationTests.kt
@@ -0,0 +1,13 @@
+package edu.ucu.statisticbuilder
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StatisticBuilderApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
