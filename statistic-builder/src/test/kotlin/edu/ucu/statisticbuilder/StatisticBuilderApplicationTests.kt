e3eed81a2393216f74b5451195a8f25611cf95f5
Yaroslav Borodaienko <yaroslaww1@gmail.com>
2023-06-14 21:10:57 +0100

    Infrastructure: Added StatisticBuilder to docker-compose

--- statistic-builder/src/test/kotlin/edu/ucu/statisticbuilder/StatisticBuilderApplicationTests.kt
+++ statistic-builder/src/test/kotlin/edu/ucu/statisticbuilder/StatisticBuilderApplicationTests.kt
@@ -1,13 +1,7 @@
 package edu.ucu.statisticbuilder
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
 class StatisticBuilderApplicationTests {
-
-    @Test
-    fun contextLoads() {
-    }
 
 }
